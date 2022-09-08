e7994dcdbce7fa8fcc88a5ffaa2d0fcb21fc2375
deimarut <deimante.rutenaite@gmail.com>
2022-09-08 23:22:04 +0300

    pirmosios uzduoties atlikimas

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,30 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const outputElement = document.getElementById('output');
+const formElement = document.getElementById('form');
+const searchElement = document.getElementById('search');
+
+formElement.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const lb = parseInt(searchElement.value) * 2.2046;
+    const g = parseInt(searchElement.value) / 0.0010000;
+    const oz = parseInt(searchElement.value) * 35.274;
+
+    const ulElement = document.createElement('ul');
+
+    const liElementLB = document.createElement('li');
+    const liElementG = document.createElement('li');
+    const liElementOZ = document.createElement('li');
+
+    liElementLB.textContent = `Jūsų nurodyti kilogramai: ${searchElement.value}, svarais bus: ${lb}`;
+    liElementG.textContent = `Jūsų nurodyti kilogramai: ${searchElement.value}, gramais bus: ${g}`;  
+    liElementOZ.textContent = `Jūsų nurodyti kilogramai: ${searchElement.value}, uncijomis bus: ${oz}`;
+
+    ulElement.appendChild(liElementOZ);
+    ulElement.appendChild(liElementG);
+    ulElement.appendChild(liElementLB);
+
+    outputElement.appendChild(ulElement);
+});
