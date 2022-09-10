6987309765f1bedc3def8513a1e2e3fff81c8948
deimarut <deimante.rutenaite@gmail.com>
2022-09-10 13:23:26 +0300

    atlikta sesta uzduotis

--- Task 6/script.js
+++ Task 6/script.js
@@ -19,3 +19,24 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30 },
   { id: '9', name: 'Daniel Cane', age: 51 },
 ];
+
+const getUserAverageAge = (arr) => {
+  const { sum, count } = arr.reduce((acc, val) => {
+    let { sum, count } = acc; 
+    sum += val.age; 
+    count++;
+    return { sum, count };
+  }, 
+  {
+    sum: 0, count: 0
+  });
+  return `The average age of al the users is ${sum / count}`;
+};
+
+console.log(getUserAverageAge(users));
+
+const getUsersNames = users.map((users) => {
+  return `${users.name}`;
+});
+
+console.log(getUsersNames);
